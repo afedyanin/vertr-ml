6dfed89cbb5baba698c1176ac76e3df6e16833f6
Anatoly Fedyanin <anfemail@gmail.com>
2025-02-02 20:47:49 +0300

    add debug

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Depends
 
 from app.controllers.prediction import prediction_router
@@ -5,11 +6,15 @@ from app.controllers.prediction import prediction_router
 app = FastAPI()
 app.include_router(prediction_router, prefix="/prediction")
 
+
 @app.get('/index')
 def index():
     return {
-            'project_name': 'project name',
-            'webmaster': 'anfemail@gmail.com',
-            'created': '2025-02-02'
-            }
+        'project_name': 'project name',
+        'webmaster': 'anfemail@gmail.com',
+        'created': '2025-02-02'
+    }
 
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
