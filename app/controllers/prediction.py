3a9f548db27a5d1aec684ca5f08119fca84cd25e
Anatoly Fedyanin <anfemail@gmail.com>
2025-02-09 09:15:09 +0300

    update

--- app/controllers/prediction.py
+++ app/controllers/prediction.py
@@ -8,16 +8,13 @@ from app.models.request.prediction import PredictionRequest
 from app.services.prediction import PredictionService
 
 prediction_router = APIRouter()
+register_single_asset_trading_env()
 
 
 @prediction_router.post('/predict')
 def predict(request: PredictionRequest) -> JSONResponse:
-
-    # TODO: use singletone
-    register_single_asset_trading_env()
     sql_config = PgSqlSettings(_env_file='../app/.env')
     service = PredictionService(sql_config)
-
     response = service.predict(request)
 
     return jsonable_encoder(response)
